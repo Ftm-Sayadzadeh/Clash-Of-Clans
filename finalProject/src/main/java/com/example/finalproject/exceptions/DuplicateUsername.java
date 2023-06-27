896434c8ef280c988611f7efda1a8b7e72df743d
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2023-06-28 02:44:01 +0430

    - Added exception for DuplicateUsername and InvalidPassword.

---
+++ finalProject/src/main/java/com/example/finalproject/exceptions/DuplicateUsername.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.exceptions;
+
+public class DuplicateUsername extends Exception{
+    public DuplicateUsername(){
+        super("Duplicate Username");
+    }
+}
---
+++ finalProject/src/main/java/com/example/finalproject/exceptions/InvalidPassword.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.exceptions;
+
+public class InvalidPassword extends Exception{
+    public InvalidPassword(){
+        super("Invalid Password");
+    }
+}
